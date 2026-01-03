8c29aa4f8f8908065de8fdf80d6403cdb9446d11
Raj Debnath <51843551+rajdebnath1988@users.noreply.github.com>
2026-01-03 10:28:05 +0530

    Initialize Spring Boot application with a home endpoint

---
+++ src/main/java/com/finsync/App.java
@@ -0,0 +1,18 @@
+package com.finsync;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "FinSync Payment Service is Active! Version 1.0";
+    }
+}
